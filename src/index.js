f8dffe3d89d168600f2793ac0656cbd0469aba06
Greg Hurrell <greg@hurrell.net>
2015-08-09 20:34:05 -0700

    Read corpus file from $PWD/yak/corpus.txt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 /**
  * Copyright 2015-present Greg Hurrell. All rights reserved.
  * Licensed under the terms of the MIT license.
@@ -7,4 +9,28 @@
 
 'use strict';
 
-export default function index() {}
+import 'babel/polyfill';
+
+import Promise from 'bluebird';
+import fs from 'fs';
+import path from 'path';
+import yargs from 'yargs';
+
+const readFile = Promise.promisify(fs.readFile);
+
+process.on('unhandledRejection', reason => {
+  throw reason;
+});
+
+(async function() {
+  const json = require('../package');
+  const options = yargs
+    .usage('Usage: $0')
+    .version(json.version)
+    .epilog(json.homepage)
+    .strict();
+  const config = options.argv;
+
+  const corpusPath = path.join('yak', 'corpus.txt');
+  const corpus = await readFile(corpusPath);
+})();
